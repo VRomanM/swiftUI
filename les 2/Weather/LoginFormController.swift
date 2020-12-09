f26125cfaaecf2f509fe734f025f98100c52493b
Roman <vromanm@mail.ru>
2020-12-09 16:10:58 +0300

    ДЗ к уроку №2

---
+++ les 2/Weather/LoginFormController.swift
@@ -0,0 +1,122 @@
+//
+//  LoginFormController.swift
+//  Weather
+//
+//  Created by Роман Вертячих on 02.12.2020.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        // Жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        // Присваиваем его UIScrollVIew
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        let checkResult = checkUserData()
+        
+        if checkResult {
+            return true
+        } else {
+            showLoginError()
+            return false
+        }
+    }
+    
+    func checkUserData() -> Bool {
+        return loginInput.text! == "admin" && passwordInput.text! == "123456"
+    }
+    
+    func showLoginError(){
+        let alert = UIAlertController(title: "Ошибка", message: "Введены неверные данные пользователя", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var loginInput: UITextField!
+    
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func IBAction(_ sender: Any) {
+        // Получаем текст логина
+        let login = loginInput.text!
+        // Получаем текст-пароль
+        let password = passwordInput.text!
+                
+        // Проверяем, верны ли они
+        if login == "admin" && password == "123456" {
+            print("успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+            
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+            
+            // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+        }
+        
+        //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+    }
+
+    @objc func hideKeyboard() {
+        self.scrollView?.endEditing(true)
+        }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+            NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+            // Второе — когда она пропадает
+            NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        navigationController?.navigationBar.isHidden = true
+        
+        }
+
+    override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        navigationController?.navigationBar.isHidden = false
+    }
+ 
+}
